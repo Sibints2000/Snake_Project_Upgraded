e3b4266d5d15e86b0041edc68819a3ed3168fcd3
Sibints2000 <sibints2000@gmail.com>
2021-08-20 20:29:25 +0530

    Eighteenth Commit

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            data.read()
         self.color("white")
         self.penup()
         self.goto(0, 270)
